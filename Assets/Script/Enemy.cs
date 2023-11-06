bc8077f74d77cc9a226e37227b3784157c3abba0
kitsunereyna <begimbetova.dana@gmail.com>
2023-11-06 20:49:31 +0600

    shooting?
    or smth

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     protected Animator anim;
     protected Rigidbody2D rb;
+    public int health = 100;
 
 
     protected virtual void Start()
@@ -19,6 +20,17 @@ public class Enemy : MonoBehaviour
         rb.velocity = Vector2.zero;
         rb.bodyType = RigidbodyType2D.Static;
         GetComponent<Collider2D>().enabled = false;
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            anim.SetTrigger("Death");
+
+            Invoke("Death()", 2); 
+        }
     }
     private void Death()
     {
